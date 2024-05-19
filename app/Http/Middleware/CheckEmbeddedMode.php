7cee7b7fb74c1ab8bb45c9a22f5b58848f23d825
mends-code <admin@mends.eu>
2024-05-19 13:30:13 +0200

    Add feature to detect embedded mode using middleware

---
+++ app/Http/Middleware/CheckEmbeddedMode.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckEmbeddedMode
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $embeddedHostUrl = config('chatwoot.embedded_host_url');
+        $referer = $request->headers->get('referer');
+        $origin = $request->headers->get('origin');
+
+        $isEmbeddedMode = false;
+
+        if ($embeddedHostUrl) {
+            if ($referer && strpos($referer, $embeddedHostUrl) === 0) {
+                $isEmbeddedMode = true;
+            } elseif ($origin && strpos($origin, $embeddedHostUrl) === 0) {
+                $isEmbeddedMode = true;
+            }
+        }
+
+        $request->attributes->set('isEmbeddedMode', $isEmbeddedMode);
+
+        return $next($request);
+    }
+}
