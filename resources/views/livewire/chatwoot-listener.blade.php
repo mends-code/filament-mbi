c7e396c491b764299fa44aea4612c05395f817ca
mends-code <admin@mends.eu>
2024-05-20 12:49:26 +0200

    debug

--- resources/views/livewire/chatwoot-listener.blade.php
+++ resources/views/livewire/chatwoot-listener.blade.php
@@ -1,17 +1,16 @@
-<div>
-    <script>
+<script>
     window.addEventListener("message", function(event) {
         console.log(JSON.stringify(event.data));
         fetch('{{ route('store-chatwoot-data') }}', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-            },
-            body: JSON.stringify(event.data)
-        }).then(response => response.json())
-          .then(data => console.log('Success:', data))
-          .catch((error) => console.error('Error:', error));
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                body: JSON.stringify(event.data)
+            }).then(response => response.json())
+            .then(data => console.log('Success:', data))
+            .catch((error) => console.error('Error:', error));
+            console.log(response.json());
     });
-    </script>
-</div>
+</script>
