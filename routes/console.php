8863be30f67fa9dcb379c818d249e5124bc751b7
mends-code <admin@mends.eu>
2024-06-28 00:27:54 +0200

    refactor(routes): update scheduling logic for ResetUnansweredConversationsJob
    - Consolidate configuration checks and job scheduling for ResetUnansweredConversationsJob
    - Change the job frequency from every minute to every five minutes

--- routes/console.php
+++ routes/console.php
@@ -5,10 +5,5 @@ use App\Jobs\DeleteDiscardedInvoicesJob;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new DeleteDiscardedInvoicesJob)->hourly();
-$resetAssigneeInterval = config('services.chatwoot.reset_assignee_interval');
 
-$resetAssigneeEnabled = config('services.chatwoot.reset_assignee_enabled');
-
-if ($resetAssigneeEnabled) {
-    Schedule::job(new ResetUnansweredConversationsJob)->everyMinute();
-}
+config('services.chatwoot.reset_assignee_enabled') && Schedule::job(new ResetUnansweredConversationsJob)->everyFiveMinutes();
